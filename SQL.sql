8206d9eccaa1ce63dc669138b977f84de4d2175b
evidal-vi <evidal@visoftware.com.co>
2025-10-27 09:35:28 -0500

    MODULO: PRUEBA PIX ROBOTICS MEJORA: PROYECTO PIX RPA

---
+++ SQL.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE prixrpa
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE SCHEMA IF NOT EXISTS public
+    AUTHORIZATION pg_database_owner;
+
+COMMENT ON SCHEMA public
+    IS 'standard public schema';
+
+GRANT USAGE ON SCHEMA public TO PUBLIC;
+
+GRANT ALL ON SCHEMA public TO pg_database_owner;
+
+CREATE TABLE PRODUCTOS (
+	id INTEGER PRIMARY KEY,
+	title TEXT,
+	price NUMERIC(10,2),
+	category TEXT,
+	description TEXT,
+	fecha_insercion TIMESTAMP
+);
